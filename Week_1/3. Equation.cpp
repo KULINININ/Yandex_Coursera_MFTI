a93cd790c1cf447bc5eb04b4b0aa0843aac6bffc
KULINININ <aleks680026032001@gmail.com>
2020-08-16 12:57:01 +0500

    Create 3. Equation.cpp

---
+++ Week_1/3. Equation.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+#include <set>
+int main()
+{
+    std::set<double> s;
+    double a,b,c, x1, x2,D;
+    std::cin >> a >> b >> c;
+    D = sqrt(pow(b, 2) - 4 * a * c);
+    if ((D >= 0) && (a!=0))
+    {
+        x1 = (-b + D) / (2 * a);
+        x2 = (-b - D) / (2 * a);
+        s.insert(x1);
+        s.insert(x2);
+        for (auto item : s)
+        {
+            std::cout << item << " ";
+        }
+    }
+    else if (a == 0)
+    {
+        std::cout << c / b;
+    }
+}
