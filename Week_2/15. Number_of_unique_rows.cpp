ec5317fb30bd2a6ba4568092c6adad592566d1fe
KULINININ <aleks680026032001@gmail.com>
2020-08-20 10:45:53 +0500

    Create 15. Number_of_unique_rows.cpp

---
+++ Week_2/15. Number_of_unique_rows.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <set>
+
+int main()
+{
+    std::set <std::string> str;
+    int input;
+    std::cin >> input;
+
+    for (size_t i = 0; i < input; i++)
+    {   
+        std::string input_;
+        std::cin >> input_;
+        str.emplace(input_);
+    }
+    
+    std::cout << str.size();
+}
