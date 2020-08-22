cc393de3b6d934e5ba5393429a8f6163d05d2f25
KULINININ <aleks680026032001@gmail.com>
2020-08-22 18:29:07 +0500

    Create 8. Rational_class.cpp

---
+++ Week_4/8. Rational_class.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+
+int GreatestCommonDivisor(int a, int b) {
+    if (b == 0) {
+        return a;
+    } else {
+        return GreatestCommonDivisor(b, a % b);
+    }
+}
+
+class Rational {
+public:
+    Rational() {
+        numenator = 0;
+        denominator = 1;
+    }
+
+    Rational(int numerator, int denominator) {
+        int gcd = GreatestCommonDivisor(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    int Numerator() const {
+        return numenator;
+    }
+
+    int Denominator() const {
+        return denominator;
+    }
+
+private:
+    int numenator;
+    int denominator;
+};
+
+int main() {
+
+using namespace std;
+
+    {
+        const Rational r(3, 10);
+        std::cout << r.Denominator() << " " << r.Numerator();
+        if (r.Numerator() != 3 || r.Denominator() != 10) {
+            cout << "Rational(3, 10) != 3/10" << endl;
+            return 1;
+        }
+    }
+
+    {
+        const Rational r(8, 12);
+        if (r.Numerator() != 2 || r.Denominator() != 3) {
+            cout << "Rational(8, 12) != 2/3" << endl;
+            return 2;
+        }
+    }
+
+    {
+        const Rational r(-4, 6);
+        if (r.Numerator() != -2 || r.Denominator() != 3) {
+            cout << "Rational(-4, 6) != -2/3" << endl;
+            return 3;
+        }
+    }
+
+    {
+        const Rational r(4, -6);
+        if (r.Numerator() != -2 || r.Denominator() != 3) {
+            cout << "Rational(4, -6) != -2/3" << endl;
+            return 3;
+        }
+    }
+
+    {
+        const Rational r(0, 15);
+        if (r.Numerator() != 0 || r.Denominator() != 1) {
+            cout << "Rational(0, 15) != 0/1" << endl;
+            return 4;
+        }
+    }
+
+    {
+        const Rational defaultConstructed;
+        if (defaultConstructed.Numerator() != 0 || defaultConstructed.Denominator() != 1) {
+            cout << "Rational() != 0/1" << endl;
+            return 5;
+        }
+    }
+
+    cout << "OK" << endl;
+    return 0;
+}
