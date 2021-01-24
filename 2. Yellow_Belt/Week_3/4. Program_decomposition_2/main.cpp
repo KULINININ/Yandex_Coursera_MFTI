b2f82b0ece516e8ad41a5cb7798abc7330815483
KULINININ <aleks680026032001@gmail.com>
2021-01-24 21:12:46 +0500

    Create 4 task

---
+++ 2. Yellow_Belt/Week_3/4. Program_decomposition_2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cassert>
+#include <map>
+#include <string>
+#include <vector>
+
+#include "query.h"
+#include "query.cpp"
+#include "responses.h"
+#include "responses.cpp"
+#include "bus_manager.h"
+#include "bus_manager.cpp"
